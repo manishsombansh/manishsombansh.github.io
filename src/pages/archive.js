0ea4676b67170da472d63f3c4103ca801b55cd97
manishsombansh <manishsombansh@gmail.com>
2020-06-12 19:43:35 -0700

    Fixed titles

--- src/pages/archive.js
+++ src/pages/archive.js
@@ -109,7 +109,7 @@ const ArchivePage = ({ location, data }) => {
   return (
     <Layout location={location}>
       <Helmet>
-        <title>Archive | Brittany Chiang</title>
+        <title>Archive | Manish Sombansh</title>
         <link rel="canonical" href="https://brittanychiang.com/archive" />
       </Helmet>
 
@@ -234,7 +234,7 @@ export const pageQuery = graphql`
             title
             tech
             github
-            external                        
+            external
             company
           }
           html
